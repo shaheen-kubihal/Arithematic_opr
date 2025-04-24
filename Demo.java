46aec9f85592009245c62e26adffb1a92066905c
shaheen-kubihal <kspatange630@gmail.com>
2025-04-24 11:14:16 +0530

    save file

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        int a=20; int b=5;
+        System.out.println("Addiytion : "+(a+b));
+        System.out.println("Substraction :"+(a-b));
+      
+    
+    }
+    }
+    
