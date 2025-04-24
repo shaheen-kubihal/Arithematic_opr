fb374358afb5ff2a64bdb3575630ab246a3258e9
Shaheen Kubihal <shaheenkubihal@gmail.com>
2025-04-24 11:21:43 +0530

    Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         int a=20; int b=5;
-        System.out.println("Addiytion : "+(a+b));
+        System.out.println("Addition : "+(a+b));
         System.out.println("Substracation :"+(a-b));
         System.out.println("Multiplication :"+(a*b));
         System.out.println("Division :"+(a/b));
